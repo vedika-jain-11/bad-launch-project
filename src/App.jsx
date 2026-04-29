b638b061e10b289023b7f49cff626d939963f4a5
Vedika Jain <vedikajain.350@gmail.com>
2026-04-28 19:24:52 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Hello from my live project!</h1>
+      <button type="button">I shipped this.</button>
+    </main>
+  )
+}
+
+export default App
